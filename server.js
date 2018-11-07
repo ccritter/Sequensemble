dd62d4dbd13889de828a8ac43f33fa50dfe2d59c
Chris <chriscritter1@gmail.com>
2018-11-06 22:04:49 -0500

    Hopefully fixed port issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(bodyParser.json());
